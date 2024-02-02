74efdcc9e3b2c710ba5c732567f9672ab4e1f4cf
Terminal <terminalkarabo@gmail.com>
2024-02-02 04:21:10 -0800

    main header file updated

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct convert - struc that function as converter for printf
+ * @conv: pointer of type for specifier
+ * @function: func conversion specifier
+ *
+ */
 
+typedef struct convert
+{
+	char *conv;
+	int (*function)();
+} converter;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
