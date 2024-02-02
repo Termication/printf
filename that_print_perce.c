7d5b3a22e72df419c651d575cdf2b5d4d197d6b1
Terminal <terminalkarabo@gmail.com>
2024-02-02 04:07:54 -0800

    function that print char percentage symbol

---
+++ that_print_perce.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_37 - Prints the character '%'.
+ *
+ * This function is designed to print the '%'
+ * character using the custom
+ * _putchar function. It is intended to be
+ * part of a larger printf-like program.
+ *
+ * Return: The number of characters printed, which is 1.
+ */
+int print_(void)
+{
+	_putchar(37);  /* Print the '%' character */
+	return (1);
+}
